5f9e4ba223b6210c0ccfdfe022cb57b5194a67fc
irfanthemis-cyber <irfanthemis@gmail.com>
2026-02-20 07:58:51 +0900

    add raft script

---
+++ raft.lua
@@ -0,0 +1,8 @@
+print("🔥 50 Days on a Raft script loaded 🔥")
+
+local player = game.Players.LocalPlayer
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Loaded",
+    Text = "Script berhasil jalan!",
+    Duration = 5
+})
