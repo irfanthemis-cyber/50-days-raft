0b38ab40d6664ed6cf753fe12c8a9dcfc6f28417
irfanthemis-cyber <irfanthemis@gmail.com>
2026-02-20 09:05:18 +0900

    Update raft.lua

--- raft.lua
+++ raft.lua
@@ -1,14 +1,1 @@
-local plr = game.Players.LocalPlayer
-local hrp = plr.Character or plr.CharacterAdded:Wait()
-hrp = hrp:WaitForChild("HumanoidRootPart")
-
-local startPos = hrp.CFrame
-
--- pindah sekali
-hrp.CFrame = hrp.CFrame * CFrame.new(0,0,-10)
-wait(1)
-
--- balik
-hrp.CFrame = startPos
-
-print("TEST SELESAI")
+print("TES BERSIH")
